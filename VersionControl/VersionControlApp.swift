6e3f820eea9008550411a842360fe094a404a356
a.m.student <tailyrdebose2017@gmail.com>
2021-01-20 08:28:22 -0600

    Initial Commit

---
+++ VersionControl/VersionControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  VersionControlApp.swift
+//  VersionControl
+//
+//  Created by A.M. Student on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct VersionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
